3bcc09c32c40f18ad6cdb89f8a6d26e723660ba7
ahmadseptian <ryanpkd10@gmail.com>
2022-01-24 01:58:45 +0700

    Sample Commit

---
+++ lib/screens/settings.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:social_media_app/utils/constants.dart';
+
+class Setting extends StatefulWidget {
+  @override
+  _SettingState createState() => _SettingState();
+}
+
+class _SettingState extends State<Setting> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: InkWell(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Icon(Icons.keyboard_backspace),
+        ),
+        backgroundColor: Theme.of(context).backgroundColor,
+        elevation: 0.0,
+        title: Text(
+          "Pengaturan",
+          style: TextStyle(),
+        ),
+      ),
+      backgroundColor: Theme.of(context).backgroundColor,
+      body: Padding(
+        padding: EdgeInsets.all(10.0),
+        child: ListView(
+          children: <Widget>[
+             ListTile(
+              title: Text(
+                "Tentang",
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+              subtitle: Text("Aplikasi Media Sosial yang Berfungsi Penuh Dibuat oleh CharlyKeleb"),
+              trailing: Icon(Icons.error)
+            ),
+            Divider(),
+            ListTile(
+              title: Text(
+                "Dark Mode",
+                style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+              subtitle: Text("gunakan dark mode"),
+              trailing: Consumer<ThemeNotifier>(
+                builder: (context, notifier, child) => CupertinoSwitch(
+                  onChanged: (val) {
+                    notifier.toggleTheme();
+                  },
+                  value: notifier.dark,
+                  activeColor: Theme.of(context).accentColor,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
